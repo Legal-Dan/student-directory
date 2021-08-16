c55287f6965d2ab0623dec915d1f9e4a9aa67092
Daniel Clements <clemies2000@hotmail.com>
2021-08-16 15:04:59 +0100

    Changed list of students to array

--- directory.rb
+++ directory.rb
@@ -1,18 +1,26 @@
-student_count = 11
 #Basic header
 puts "The students of Villains Academy"
 puts "---------------"
+
 #List of students
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wiched Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students = [
+  "Dr. Hannibal Lecter", 
+  "Darth Vader", 
+  "Nurse Ratched", 
+  "Michael Corleone", 
+  "Alex DeLarge", 
+  "The Wiched Witch of the West", 
+  "Terminator", 
+  "Freddy Krueger", 
+  "The Joker", 
+  "Joffrey Baratheon", 
+  "Norman Bates"
+]
+
+#Print out student names
+students.each do |student|
+  puts student
+end
+
 #Student count
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.count} great students"
