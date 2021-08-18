b5e5494a73e732e136fac23c04b8c07522aa34bb
Daniel Clements <clemies2000@hotmail.com>
2021-08-18 15:18:11 +0100

    Make like manually fillable

--- directory.rb
+++ directory.rb
@@ -5,19 +5,18 @@ def print_header
 end
 
 #List of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wiched Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def input_students
+  students = []
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 #Print out student names
 def print(names)
@@ -31,6 +30,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
